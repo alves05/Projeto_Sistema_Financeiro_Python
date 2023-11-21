41a2dbc2e8ebbe43588c00246f0712cd0595c018
Tamires <tamires.crisouza@gmail.com>
2023-11-20 22:39:07 -0300

    código alterar com as correções

--- codigo/control_finance.py
+++ codigo/control_finance.py
@@ -1,41 +1,47 @@
-import csv
-import datetime
-
-movimentacoes = []   #lista que armazena as movimentacoes realizadas
- 
 def atualizar_movimentacao():
-     exibir_movimentacoes()   # antes de da função atualizar criar uma função para exibir as movimentações já realizadas, deverá conter o número (índice) da movimentação
-     if not movimentacoes:    # caso não seja encontrada nenhum registro no arquivo csv, retorna "Nenhuma movimentação foi encontrada"
-         print("Nenhuma movimentação econtrada")
-         
-     indice = int(input("Digite o número da movimentação que deseja atualizar: "))
-     if 0 <= indice < len(movimentacoes):
-         tipo = input("Digite o novo tipo de movimentação (receita, despesa, investimento): ") # resgistra o novo tipo
-         data = datetime.date.today().strftime("%Y-%m-%d") #atualiza data para data atual
-         
-         if tipo.lower() == 'despesa':
-             valor = -float(input("Digite o novo valor da movimentação: "))
-             montante = 0
-             taxa = 0
-             rendimento = 0
-             
-         elif tipo.lower() == 'investimento':
-             valor = float(input("Digite o novo valor da movimentação: "))
-             taxa = float(input("Digite a nova taxa de rendimento (em decimal): "))
-             tempo_dias = (datetime.date.today() - datetime.datetime.strptime(data, "%Y-%m-%d").date()).days
-             montante = round(valor * (1 + taxa) ** tempo_dias, 2)  
-             rendimento = round(montante - valor, 2)
-             
-         else:
+    with open('base.csv', 'r') as arquivo:
+        leitor = csv.reader(arquivo, delimiter=';')
+        movimentacao = list(leitor)
+
+    if not movimentacao:
+        pass
+
+    indice = int(input("Digite o número da movimentação que deseja atualizar: "))
+
+    if 0 <= indice < len(movimentacao):
+        tipo = input("Digite o novo tipo de movimentação (receita, despesa, investimento): ")
+        data = datetime.date.today().strftime("%Y-%m-%d")
+
+        if tipo.lower() == 'despesa':
+            valor = -float(input("Digite o novo valor da movimentação: "))
+            montante = 0
+            taxa = 0
+            rendimento = 0
+            historico = ''
+        elif tipo.lower() == 'investimento':
+            valor = float(input("Digite o novo valor investido: "))
+            taxa = float(input("Digite a nova taxa de rendimento (em decimal): "))
+            tempo_dias = (datetime.date.today() - datetime.datetime.strptime(data, "%Y-%m-%d").date())
+            montante = round(valor * (1 + taxa) ** tempo_dias.days, 2)
+            rendimento = round(montante - valor, 2)
+            historico = ''
+        else:
             valor = float(input("Digite o novo valor da movimentação: "))
             montante = 0
             taxa = 0
             rendimento = 0
-             
-        # falta criar código para atualizar a lista movimentações  
-        print("Movimentação atualizada com sucesso")
-    else:
-        print("Índice inválido")
+            historico = ''
+
+        movimentacao[indice] = [
+            str(indice), str(int(data.split('-')[2])), str(int(data.split('-')[1])), str(int(data.split('-')[0])),
+            tipo, valor, historico, taxa, montante, 
+            rendimento] if tipo.lower() == 'investimento' else [
+                                                                str(indice),str(int(data.split('-')[2])),
+                                                                str(int(data.split('-')[1])),
+                                                                str(int(data.split('-')[0])),tipo,valor,historico,
+                                                                '','','']
+                                                                              
+        return movimentacao
         
              
          
